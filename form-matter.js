50990e84f1a36bce85ce2ee3229dfe8941c6e9b8
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-12-16 06:27:29 -0500

    ...

--- form-matter.js
+++ form-matter.js
@@ -3,10 +3,19 @@ import { define } from 'xtal-element/XtalElement.js';
 export class FormMatter extends XtalDecor {
     constructor() {
         super(...arguments);
+        this.formMap = new WeakMap();
         this.on = {
             submit: ({ self }, e) => {
                 console.log(e);
                 e.preventDefault();
+                const frm = e.target;
+                if (!this.formMap.has(frm)) {
+                    const formExtensionData = {
+                        originalAction: frm.action,
+                    };
+                    this.formMap.set(frm, formExtensionData);
+                }
+                frm.submit();
             }
         };
         this.actions = [];
--- form-matter.ts
+++ form-matter.ts
@@ -1,13 +1,25 @@
 import {XtalDecor} from 'xtal-decor/xtal-decor.js';
 import {define} from 'xtal-element/XtalElement.js';
 
+export interface FormExtensionData {
+    originalAction: string;
+}
+
 export class FormMatter extends XtalDecor {
-    static is = 'form-matter'
-    
+    static is = 'form-matter';
+    formMap = new WeakMap<HTMLFormElement, FormExtensionData>();
     on = {
         submit: ({self}: any, e: Event) => {
             console.log(e);
             e.preventDefault();
+            const frm = e.target as HTMLFormElement;
+            if(!this.formMap.has(frm)){
+                const formExtensionData = {
+                    originalAction: frm.action,
+                } as FormExtensionData;
+                this.formMap.set(frm, formExtensionData);
+            }
+            frm.submit();
         }
     }
     actions = [];
