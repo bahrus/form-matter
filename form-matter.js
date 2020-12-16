1d9e641947026116cb7d476afb94788549bff855
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-12-16 06:58:01 -0500

    ...

--- form-matter.js
+++ form-matter.js
@@ -1,5 +1,16 @@
 import { XtalDecor } from 'xtal-decor/xtal-decor.js';
 import { define } from 'xtal-element/XtalElement.js';
+//https://stackoverflow.com/a/43537400/3320028
+export function splitPairs(str, lhsDelim, rhsDelim) {
+    const array = [];
+    let c = 0;
+    const regExp = new RegExp(`([${lhsDelim}${rhsDelim}])`, 'i');
+    str.split(regExp).filter(Boolean).forEach(e => 
+    // Increase / decrease counter and push desired values to an array
+    e == '(' ? c++ : e == ')' ? c-- : c > 0 ? array.push('(' + e + ')') : array.push(e));
+    return array;
+}
+//const balBraces = /\{([^\]]+)}/g;
 export class FormMatter extends XtalDecor {
     constructor() {
         super(...arguments);
@@ -10,12 +21,23 @@ export class FormMatter extends XtalDecor {
                 e.preventDefault();
                 const frm = e.target;
                 if (!this.formMap.has(frm)) {
+                    const action = frm.action;
+                    //let regExpArray: RegExpMatchArray | null = null;
+                    if (action) {
+                        //regExpArray = action.match(balBraces);
+                        const action2 = decodeURI(action);
+                        const splitPairArr = splitPairs(action2, '{', '}');
+                        debugger;
+                    }
                     const formExtensionData = {
-                        originalAction: frm.action,
+                        originalAction: action,
                     };
                     this.formMap.set(frm, formExtensionData);
                 }
-                frm.submit();
+                const frmExtData = this.formMap.get(frm);
+                if (frmExtData.originalAction) {
+                }
+                //frm.submit();
             }
         };
         this.actions = [];
--- form-matter.ts
+++ form-matter.ts
@@ -3,8 +3,20 @@ import {define} from 'xtal-element/XtalElement.js';
 
 export interface FormExtensionData {
     originalAction: string;
+    regExpArray: RegExpMatchArray;
 }
-
+//https://stackoverflow.com/a/43537400/3320028
+export function splitPairs(str: String, lhsDelim: string, rhsDelim: string): string[]{
+    const array: string[] = [];
+    let c = 0;
+    const regExp = new RegExp(`([${lhsDelim}${rhsDelim}])`, 'i');
+    str.split(regExp).filter(Boolean).forEach(e =>
+        // Increase / decrease counter and push desired values to an array
+        e == '(' ? c++ : e == ')' ? c-- : c > 0 ? array.push('(' + e + ')') : array.push(e)
+    );
+    return array;
+}
+//const balBraces = /\{([^\]]+)}/g;
 export class FormMatter extends XtalDecor {
     static is = 'form-matter';
     formMap = new WeakMap<HTMLFormElement, FormExtensionData>();
@@ -14,12 +26,25 @@ export class FormMatter extends XtalDecor {
             e.preventDefault();
             const frm = e.target as HTMLFormElement;
             if(!this.formMap.has(frm)){
+                const action = frm.action;
+                //let regExpArray: RegExpMatchArray | null = null;
+                if(action){
+                    //regExpArray = action.match(balBraces);
+                    const action2 = decodeURI(action);
+                    const splitPairArr = splitPairs(action2, '{', '}');
+                    debugger;
+                }
                 const formExtensionData = {
-                    originalAction: frm.action,
+                    originalAction: action,
+                    //regExpArray: regExpArray,
                 } as FormExtensionData;
                 this.formMap.set(frm, formExtensionData);
             }
-            frm.submit();
+            const frmExtData = this.formMap.get(frm)!;
+            if(frmExtData.originalAction){
+
+            }
+            //frm.submit();
         }
     }
     actions = [];
